77f381d3a38fd5ba3a8035b42aa91d81bad54d74
Correct Username <correct@example.com>
2024-08-21 16:49:44 +0530

    added something

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -3,40 +3,47 @@ import styled from 'styled-components';
 import { IoCall } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
-
+import toast, { Toaster } from 'react-hot-toast';
 
 function Contact() {
-
     const onSubmit = async (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
 
-
-        formData.append("access_key", "422a0498-898f-4912-813f-ed36aaf872cf");
-
+        // Convert form data to JSON
         const object = Object.fromEntries(formData);
         const json = JSON.stringify(object);
 
-        const res = await fetch("", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            },
-            body: json
-        }).then((res) => res.json());
+        try {
+            const res = await fetch("https://portfolio-backend-ebon-five.vercel.app/sendemail", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                },
+                body: json
+            });
 
-        if (res.success) {
-            console.log("Success", res);
-            alert("Form submitted successfully!. Please Check tha mail");
-        } else {
-            console.error("Error:", res.message);
-            alert("Error:" + res.message);
+            const result = await res.json();
+
+            if (res.ok) {
+                console.log("Success", result);
+                notify()
+            } else {
+                console.error("Error:", result.message);
+                alert("Error: " + result.message);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            alert("An error occurred while sending the message.");
         }
     };
 
+    const notify = () => toast.success('Message sent to Sherin S');
+
     return (
         <>
+        <Toaster/>
             <ContactMain>
                 <h1>Contact</h1>
                 <ContactSection>
@@ -59,19 +66,17 @@ function Contact() {
                     <ContactRight>
                         <form onSubmit={onSubmit}>
                             <label className='label'>Name</label><br />
-                            <input name='userName' type='text' placeholder='Enter your name' /><br />
+                            <input name='userName' type='text' placeholder='Enter your name' required /><br />
                             <label className='label'>Email</label><br />
-                            <input name='email' type='email' placeholder='Enter the email' /><br />
+                            <input name='email' type='email' placeholder='Enter your email' required /><br />
                             <label className='label'>Write Your message here</label><br />
-                            <textarea name='message' rows="8" placeholder='Enter your message'></textarea>
-                            <Button type="submit" >Submit</Button>
+                            <textarea name='message' rows="8" placeholder='Enter your message' required></textarea>
+                            <Button type="submit">Submit</Button>
                         </form>
                     </ContactRight>
                 </ContactSection>
             </ContactMain>
-
         </>
-
     );
 }
 
@@ -83,29 +88,23 @@ const ContactMain = styled.div`
   padding: 2rem;
   margin: 2rem 8rem ;
   margin-bottom: 0px;
-  
-
   h1 {
     font-size: 3rem;
     text-align: center;
     font-weight: 500;
   }
-  
-@media(max-width:1024px){
-  margin:0 4rem;
- 
-}
-@media(max-width:640px){
-  margin:0 2rem ;
-  padding:0;
-  
-}
-@media(max-width:460px){
-  margin-bottom: 0px;
-  padding:0;
-  margin:0 2rem ;
-
-}
+  @media(max-width:1024px){
+    margin:0 4rem;
+  }
+  @media(max-width:640px){
+    margin:0 2rem ;
+    padding:0;
+  }
+  @media(max-width:460px){
+    margin-bottom: 0px;
+    padding:0;
+    margin:0 2rem ;
+  }
 `;
 
 const ContactSection = styled.div`
@@ -114,24 +113,21 @@ const ContactSection = styled.div`
   align-items: center;
   margin-top: 60px;
   margin-bottom: 75px;
-
   @media(max-width:1257px){
     gap: 80px;
     flex-direction: column;
     margin-top: 169px;
-   
- 
   }
 `;
 
 const ContactLeft = styled.div`
-margin-top: -100px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-@media(max-width:460px){
- max-width:100%;       
-}
+  margin-top: -100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media(max-width:460px){
+    max-width:100%;       
+  }
   h5 {
     font-size: 3rem;
     color: #f59e0b;
@@ -140,8 +136,7 @@ margin-top: -100px;
       font-size: 2.4rem;
       display: none;
     }
-    
-
+  }
   p {
     font-size: 1.2rem;
     margin: 20px 0;
@@ -159,11 +154,9 @@ const ContactList = styled.div`
   align-items: center;
   gap: 20px;
   margin: 1rem 0;
-
   .icons {
     font-size: 1.6rem;
   }
-
   h6 {
     font-size: 1.2rem;
   }
@@ -173,20 +166,16 @@ const ContactRight = styled.div`
   box-shadow: 0 0 30px #f59e0b;
   border-radius: 2rem;
   padding-bottom: 2rem;
-  
-
   form {
     display: flex;
     flex-direction: column;
     padding: 2rem;
   }
-
   .label {
     color: #D8D8D8;
     font-size: 18px;
     font-weight: 500;
   }
-
   input,
   textarea {
     border: none;
@@ -202,7 +191,6 @@ const ContactRight = styled.div`
     @media(max-width:460px){
       width: 217px;
       height: 32px;
-
     }
   }
   textarea {
@@ -210,7 +198,6 @@ const ContactRight = styled.div`
     @media(max-width:460px){
       height: 65px;
     }
-  
   }
 `;
 
@@ -226,12 +213,9 @@ const Button = styled.button`
   transition: background-color 0.3s ease;
   @media(max-width:460px){
     width: 93px;
-  height: 36px;
-  margin-top: 10px;
+    height: 36px;
+    margin-top: 10px;
   }
-  
-
-
   &:hover {
     background-color: #d18f00;
   }
