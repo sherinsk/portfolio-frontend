70de88ecf45a0a181b92fa8d951712039bc0445c
Correct Username <correct@example.com>
2024-08-21 22:24:42 +0530

    added loading..

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import Spinning from '../loading/Spinner';
+// import Spinning from '../loading/Spinner';
 
 function Projects() {
   const [projects, setProjects] = useState(null);
@@ -29,7 +29,7 @@ function Projects() {
         <h1>Projects</h1>
         {loading ? (
           <LoadingContainer>
-            <Spinning/>
+            <p className='text-white'>Loading.....</p>
           </LoadingContainer>
         ) : (
           <Project>
--- src/loading/Spinner.jsx
+++ src/loading/Spinner.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import Spinner from 'react-bootstrap/Spinner';
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import Spinner from 'react-bootstrap/Spinner';
 
-export default function Spinning() {
-  return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '70vh', // Full viewport height
-      width: '100%', // Full width of the container
-      padding: 30,
-      margin: "auto"
-    }}> 
-      <Spinner animation="border" variant="primary" /> 
-    </div>    
-  );
-}
+// export default function Spinning() {
+//   return (
+//     <div style={{
+//       display: 'flex',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       height: '70vh', // Full viewport height
+//       width: '100%', // Full width of the container
+//       padding: 30,
+//       margin: "auto"
+//     }}> 
+//       <Spinner animation="border" variant="primary" /> 
+//     </div>    
+//   );
+// }
